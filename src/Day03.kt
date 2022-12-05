a90864e0f2797f3d4b4bb35f101a7e5f35c33a2a
Ben Ooms <benoomsdh@gmail.com>
2022-12-05 19:56:26 +0100

    refactored day 3 for readability

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,31 +1,32 @@
 fun main() {
 
+    fun charToValue(char: Char) = if (char.code >= 97) char.code - 96 else char.code - 38
+
     fun part1(input: List<String>): Int {
-        var combinedPrio = 0
-        input.forEach { rucksack ->
-            val chuncks = rucksack.chunked(rucksack.length / 2).map { it.toCharArray() }.toTypedArray()
-            combinedPrio += chuncks[0].intersect(chuncks[1].toSet()).map {
-                if (it.code >= 97) it.code - 96 else it.code - 38
-            }.sum()
+        return input.sumOf { rucksack ->
+            val chunks = rucksack.chunked(rucksack.length / 2).map { it.toCharArray() }.toTypedArray()
+            val compartment1 = chunks[0].toSet()
+            val compartment2 = chunks[1].toSet()
+
+            (compartment1 intersect compartment2).sumOf {
+                charToValue(it)
+            }
         }
-        return combinedPrio
     }
 
     fun part2(input: List<String>): Int {
         var totalPrio = 0
-        input.map { it.toCharArray() }.chunked(3).forEach { group ->
-            totalPrio += group[0].intersect(group[1].toSet())
-                .intersect(group[2].toSet())
-                .map { char->
-                    println(char)
-                    if (char.code >= 97) char.code - 96 else char.code - 38
-                }.sum()
+        input.map { it.toCharArray().toSet() }.chunked(3).forEach { group ->
+            totalPrio += (group[0] intersect group[1] intersect group[2]).sumOf {
+                    char -> charToValue(char)
+            }
         }
         return totalPrio
     }
 
     // test if implementation meets criteria from the description, like:
      val testInput = readInput("Day03_test")
+     check(part1(testInput) == 157)
      check(part2(testInput) == 70)
 //
     val input = readInput("Day03")
